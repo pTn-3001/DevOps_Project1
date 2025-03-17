7e7d881a644bbea25100633b5475a2a04b749fea
pTn-3001 <trungnguyenphan3001@gmail.com>
2025-03-17 10:00:49 +0700

    Update VetRepositoryTest.java

--- spring-petclinic-vets-service/src/test/java/org/springframework/samples/petclinic/vets/model/VetRepositoryTest.java
+++ spring-petclinic-vets-service/src/test/java/org/springframework/samples/petclinic/vets/model/VetRepositoryTest.java
@@ -5,7 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import static org.junit.jupiter.api.Assertions.*;
-
+// Hello
 @DataJpaTest
 class VetRepositoryTest {
 
