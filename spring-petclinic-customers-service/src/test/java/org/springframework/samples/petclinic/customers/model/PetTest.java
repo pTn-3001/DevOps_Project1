2e8764ad3607d05c3fbfc3c97a7c7434f3687e80
pTn-3001 <trungnguyenphan3001@gmail.com>
2025-03-19 09:26:50 +0700

    Update PetTest.java

--- spring-petclinic-customers-service/src/test/java/org/springframework/samples/petclinic/customers/model/PetTest.java
+++ spring-petclinic-customers-service/src/test/java/org/springframework/samples/petclinic/customers/model/PetTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.Date;
-
+// Hello
 import static org.junit.jupiter.api.Assertions.*;
 
 class PetTest {
